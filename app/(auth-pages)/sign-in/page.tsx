093cc5dd2492ca873a2420d9f28e7cd1bb8f9c55
parunchxi <parunchxi@gmail.com>
2025-04-19 20:14:59 +0700

    chore: add code formatting and linting tools

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,17 +1,17 @@
-import { signInAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
+import { signInAction } from '@/app/actions'
+import { FormMessage, Message } from '@/components/form-message'
+import { SubmitButton } from '@/components/submit-button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import Link from 'next/link'
 
 export default async function Login(props: { searchParams: Promise<Message> }) {
-  const searchParams = await props.searchParams;
+  const searchParams = await props.searchParams
   return (
     <form className="flex-1 flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">Sign in</h1>
       <p className="text-sm text-foreground">
-        Don't have an account?{" "}
+        Don't have an account?{' '}
         <Link className="text-foreground font-medium underline" href="/sign-up">
           Sign up
         </Link>
@@ -40,5 +40,5 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
         <FormMessage message={searchParams} />
       </div>
     </form>
-  );
+  )
 }
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,17 +1,17 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
-import { createClient } from "@/utils/supabase/server";
-import { InfoIcon } from "lucide-react";
-import { redirect } from "next/navigation";
+import FetchDataSteps from '@/components/tutorial/fetch-data-steps'
+import { createClient } from '@/utils/supabase/server'
+import { InfoIcon } from 'lucide-react'
+import { redirect } from 'next/navigation'
 
 export default async function ProtectedPage() {
-  const supabase = await createClient();
+  const supabase = await createClient()
 
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
 
   if (!user) {
-    return redirect("/sign-in");
+    return redirect('/sign-in')
   }
 
   return (
@@ -34,5 +34,5 @@ export default async function ProtectedPage() {
         <FetchDataSteps />
       </div>
     </div>
-  );
+  )
 }
