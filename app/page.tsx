66001b9fe992cb59215fe6a26d21871aa819d67c
parunchxi <parunchxi@gmail.com>
2025-05-08 23:29:03 +0700

    fix(home): display Hero instead of user data

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { createClient } from '@/utils/supabase/server';
-import { getUserRole } from '@/utils/getRoles';
-export default async function Users() {
-  const supabase = await createClient();
-  const { data: users } = await supabase.from("users").select();
-  const role = await getUserRole();
+import Hero from '@/components/hero'
+
+export default async function Home() {
   return (
-    <div>
-      <h1>Role: {role}</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-    </div>
-  );
+    <>
+      <Hero />
+    </>
+  )
 }
