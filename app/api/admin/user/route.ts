c05d992c3d766c63939fff0375c83a2ac14f50f1
Slozzzy <84764459+Slozzzzy@users.noreply.github.com>
2025-05-11 07:49:37 +0700

    feat(staff-api): implement GET endpoint to list staff by type with role validation

---
+++ app/api/admin/user/route.ts
@@ -0,0 +1,49 @@
+// app/api/admin/staff/route.ts
+import { NextResponse } from 'next/server'
+import { getUserRole } from '@/utils/getRoles'
+import { createClient } from '@/utils/supabase/server'
+
+const validStaffTypes = ['Doctor', 'Nurse', 'Pharmacist', 'Admin']
+const validEmploymentStatus = ['Active', 'On_Leave', 'Resigned', 'Retired']
+
+// GET /api/admin/staff → List all staff by type
+export async function GET(req: Request) {
+    const result = await getUserRole()
+    if (!result)
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+
+    const { role, userId } = result
+    if (role !== 'Admin') {
+        return NextResponse.json(
+            { error: 'Forbidden: Admin only' },
+            { status: 403 },
+        )
+    }
+
+    const staffType = new URL(req.url).searchParams.get('type')
+    const supabase = await createClient()
+
+    const { data: medicalStaff, error: medicalStaffError } = await supabase
+        .from('medical_staff')
+        .select('user_id');
+
+    if (medicalStaffError) {
+        return NextResponse.json({ error: medicalStaffError.message }, { status: 500 });
+    }
+
+    const excludedUserIds = medicalStaff && medicalStaff.length > 0
+        ? `(${medicalStaff.map((staff: any) => staff.user_id).join(',')})`
+        : '(NULL)';
+
+    let query = supabase
+        .from('users')
+        .select('*')
+        .not('user_id', 'in', excludedUserIds)
+
+    if (staffType) query = query.eq('staff_type', staffType)
+
+    const { data, error } = await query
+
+    if (error) return NextResponse.json({ error: error.message }, { status: 500 })
+    return NextResponse.json(data)
+}
