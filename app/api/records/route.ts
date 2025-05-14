105139531bf41f6f6aa86fd28073f294370f3175
Plugs <plugs2004@gmail.com>
2025-05-14 22:11:50 +0700

    feat: add GET endpoint to retrieve canceled medical records with role-based access control

--- app/api/records/route.ts
+++ app/api/records/route.ts
@@ -1,6 +1,7 @@
-import { NextResponse } from 'next/server'
 import { createClient } from '@/utils/supabase/server'
 import { getUserRole } from '@/utils/get-role'
+import { NextRequest, NextResponse } from "next/server";
+
 
 // POST /api/records → Create new medical record (Doctor only)
 export async function POST(req: Request) {
@@ -101,4 +102,52 @@ export async function POST(req: Request) {
   }
 
   return NextResponse.json(data, { status: 201 }) // Successfully created medical record
+}
+
+// GET -> get canceled medical records
+export async function GET(request: NextRequest) {
+  const supabase = await createClient();
+  const result = await getUserRole();
+
+  if (!result) {
+      return NextResponse.json(
+          { error: "Unauthorized" },
+          { status: 401 }
+      );
+  }
+
+  const { role, userId } = result;
+
+  if (role !== "Doctor" && role !== "Admin") {
+      return NextResponse.json(
+          { error: "Only for admin doctor " },
+          { status: 403 }
+      );
+  }
+
+  const { data, error } = await supabase
+      .from('medical_records')
+      .select(`
+      *,
+      medical_staff(
+          users(
+              first_name,
+              last_name
+          )
+      ),
+      patients(
+          users(
+              first_name,
+              last_name
+          )
+      )
+      `)
+      .eq('visit_status', 'Canceled')
+      .order('visit_date', { ascending: false });
+
+  if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  return NextResponse.json({ data }, { status: 200 });
 }
